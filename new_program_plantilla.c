f6d1f6d5e7f016db4a616be9bde74e371c605b78
protos50 <francojzini@gmail.com>
2021-09-05 17:26:37 -0300

    creacion proyecto integrador AED2

---
+++ new_program_plantilla.c
@@ -0,0 +1,18 @@
+/* Escribir una función generarVectorPares que genere un vector con los números pares comprendidos entre 1 y 100. Luego, escribir una función maximo que reciba un vector como
+ parámetro y retorne el máximo elemento del arreglo. Probar la función maximo en la función principal, utilizando el vector de números pares generado anteriormente. */
+#include <stdio.h>
+
+void esperarIntro(void);
+
+
+int main(void){	
+
+	esperarIntro();
+	return 0;
+}
+
+void esperarIntro(void){
+	fflush(stdin);
+	puts("\n\nPulsar Intro para continuar...");
+	getchar();
+}	
