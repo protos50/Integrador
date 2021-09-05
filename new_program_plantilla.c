c7ef039070171fb688305fe827406b61a98a6833
protos50 <francojzini@gmail.com>
2021-09-05 17:33:24 -0300

    creacion .c del codigo fuente project

--- new_program_plantilla.c
+++ new_program_plantilla.c
@@ -1,5 +1,4 @@
-/* Escribir una función generarVectorPares que genere un vector con los números pares comprendidos entre 1 y 100. Luego, escribir una función maximo que reciba un vector como
- parámetro y retorne el máximo elemento del arreglo. Probar la función maximo en la función principal, utilizando el vector de números pares generado anteriormente. */
+/* */
 #include <stdio.h>
 
 void esperarIntro(void);
