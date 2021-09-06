a40f09f168272a4afb522dc9d3de0b851f652193
protos50 <francojzini@gmail.com>
2021-09-06 19:15:02 -0300

    funcion, mensaje de bienvenida

--- Integrador.c
+++ Integrador.c
@@ -1,12 +1,28 @@
 /* Proyecto Integrador - 1ra. Etapa */
 #include <stdio.h>
+#include <string.h>
+#define maxChar 50
 
-void esperarIntro(void);
+void esperarIntro(void), mensajeBienvenida(void); 
+
+typedef char tString[maxChar];
 
 int main(void)
 {
+	mensajeBienvenida();
 	esperarIntro();
 	return 0;
+}
+
+void mensajeBienvenida(void) {
+	int i = 0;
+	tString msgEntrada = "Bienvenidos al sistema \n 'FOOD NOTIFIER'";
+	for (i = 0; i < strlen(msgEntrada); i++)
+	{
+		printf("%c", msgEntrada[i]);
+		Sleep(50);
+	}
+	
 }
 
 void esperarIntro(void)
@@ -15,3 +31,4 @@ void esperarIntro(void)
 	puts("\n\nPulsar Intro para continuar...");
 	getchar();
 }
+
